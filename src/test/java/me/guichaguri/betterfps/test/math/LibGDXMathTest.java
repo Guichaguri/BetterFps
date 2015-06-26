292ddc47b82434fc6828389871b1090938dff9c1
Guichaguri <guichaguri@hotmail.com>
2015-06-26 20:10:52 -0300

    Added JUnit Tests

---
+++ src/test/java/me/guichaguri/betterfps/test/math/LibGDXMathTest.java
@@ -0,0 +1,26 @@
+package me.guichaguri.betterfps.test.math;
+
+import me.guichaguri.betterfps.math.LibGDXMath;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Guilherme Chaguri
+ */
+public class LibGDXMathTest {
+
+    @Test
+    public void testSine() {
+        float sine = LibGDXMath.sin(1);
+
+        Assert.assertEquals(0.8414F, sine, 0.005F);
+    }
+
+    @Test
+    public void testCosine() {
+        float cosine = LibGDXMath.cos(1);
+
+        Assert.assertEquals(0.5403F, cosine, 0.005F);
+    }
+
+}
